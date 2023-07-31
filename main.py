04c264a8d9fd44aac661f0d7802b4b6d8aea9939
iamnajib <iam.najib@yahoo.com>
2023-07-31 21:26:23 +0600

    jhjh

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+
+jhjhj
